1ea677191a0375e409d78889f8475a00364b5caa
PelinSuK <pelin.su.k@hotmail.com>
2021-06-05 11:00:17 +0300

    1view

---
+++ resources/views/q11.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Pelin Su Karabacak - Final</title>
+    </head>
+    <body>
+        <h3> Pelin Su Karabacak - Final </h3>
+    </body>
+</html>
